800ce79ffb66b983c22600fb8289307d8e46eadf
Enyang Zhang <ezh15@uclive.ac.nz>
2024-02-12 21:42:24 +1300

    DT-10 [1h] Using asp.net core Web api and created model for storing artical information. Used Sqlsugar for ORM.

---
+++ Blog.Model/ArticalId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace Blog.Model
+{
+    /// <summary>
+    /// Represents the model for storing article IDs.
+    /// </summary>
+    public class ArticalId
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the article.
+        /// </summary>
+        [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+        public int Id {
+            get;
+            set;
+        }
+    }
+}
